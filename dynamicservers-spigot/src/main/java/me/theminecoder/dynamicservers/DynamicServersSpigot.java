5855a9b570fb2a1fe42149dcfd73bd762182fc71
theminecoder <theminecoder.dev@gmail.com>
2022-04-14 08:54:15 +1000

    as it turns out bukkit removed server id from server.properties.....

--- dynamicservers-spigot/src/main/java/me/theminecoder/dynamicservers/DynamicServersSpigot.java
+++ dynamicservers-spigot/src/main/java/me/theminecoder/dynamicservers/DynamicServersSpigot.java
@@ -5,6 +5,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import redis.clients.jedis.Jedis;
 
+import java.io.File;
+import java.io.FileReader;
+import java.util.Properties;
+
 public final class DynamicServersSpigot extends JavaPlugin {
 
     @Override
@@ -18,10 +22,20 @@ public final class DynamicServersSpigot extends JavaPlugin {
                 this.getLogger()
         );
 
+        String detectedServerId = new File(".").getAbsoluteFile().getName();
+        try {
+            Properties serverProperties = new Properties();
+            serverProperties.load(new FileReader("server.properties"));
+            if(serverProperties.containsKey("server-name")) detectedServerId = serverProperties.getProperty("server-name");
+            if(serverProperties.containsKey("server-id")) detectedServerId = serverProperties.getProperty("server-id");
+        } catch (Exception ignored) {
+        }
+
+        String finalDetectedServerId = detectedServerId;
         this.getServer().getScheduler().runTaskTimerAsynchronously(this, () -> {
             try (Jedis jedis = DynamicServersCore.getJedisPool().getResource()) {
                 jedis.publish(DynamicServersCore.REDIS_CHANNEL, DynamicServersCore.getGSON().toJson(new ServerData(
-                        this.getConfig().getString("server-id", Bukkit.getServerId()),
+                        this.getConfig().getString("server-id", finalDetectedServerId),
                         this.getServer().getIp() + ":" + this.getServer().getPort(),
                         this.getServer().getOnlinePlayers().size(),
                         this.getServer().getMaxPlayers()
